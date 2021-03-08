e9a3a2418993d3e154a0ebd923bc7bb2f853171d
AlvitoAuliansyah <alauliansyah01@gmailcom>
2021-03-08 22:34:11 +0700

    Commit 1 (publish projek ke dalam github (publish the project to github))

---
+++ app/src/main/java/com/example/myapplication4/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.myapplication4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean table_flg = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void collapseTable(View view) {
+        TableLayout table = findViewById(R.id.table);
+        Button switchBtn = findViewById(R.id.switchBtn);
+
+        table.setColumnCollapsed(1, table_flg);
+        table.setColumnCollapsed(2, table_flg);
+
+        if (table_flg) {
+            // CLose
+            table_flg = false;
+            switchBtn.setText("Show Detail");
+        } else {
+            // Open
+            table_flg = true;
+            switchBtn.setText("Hide Detail");
+        }
+
+    }
+
+}
